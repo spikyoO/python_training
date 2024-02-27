5d09bf06a90cb1c340841fac8a8f67e220d12576
4rrtem <testqa.artem1111@gmail.com>
2024-02-27 22:30:42 +0300

    Add: conftest.py - moving fixture to a separate file, moving file up a level to avoid bugs; test_del_group.py - adding new test case, deleting first group; Modify: modified files to move fixture;

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-from model.group import Group
-
-def test_add_group(app):
+def test_del_first_group(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
     app.group.open_group_page()
-    app.group.create(Group(header="wasd", footer="wasd", name="wasd"))
+    app.group.delete_first_group()
+    app.group.return_to_group_page()
     app.session.logout()
-
-
