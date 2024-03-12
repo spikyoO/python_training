f937ca16ba21c1068b80312d35f6373fa7ed5a1d
4rrtem <testqa.artem1111@gmail.com>
2024-03-12 16:26:59 +0300

    Adding new checks for group creation and deletion, adding new steps for allure report;

--- fixture/application.py
+++ fixture/application.py
@@ -3,6 +3,7 @@ from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
+
 class Application: 
     
     def __init__(self):
--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,8 @@
+import allure
+from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
+from model.group import Group
+
 
 class GroupHelper: 
     def __init__(self, app):
@@ -7,6 +11,10 @@ class GroupHelper:
     def open_group_page(self):
         driver = self.app.driver
         driver.find_element(By.LINK_TEXT, "groups").click()
+        with allure.step("Opening group page"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="GroupPage",
+                          attachment_type=AttachmentType.PNG)
     
     def create(self, group):
         driver = self.app.driver
@@ -14,6 +22,10 @@ class GroupHelper:
         driver.find_element(By.NAME, "new").click()
         self.fill_group_form(group)
         driver.find_element(By.NAME, "submit").click()
+        with allure.step("Group is created"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="GroupCreated",
+                          attachment_type=AttachmentType.PNG)
         self.return_to_group_page()
         
     def change_field_value(self, field_name, text):
@@ -27,16 +39,28 @@ class GroupHelper:
         self.change_field_value("group_name", group.name)
         self.change_field_value("group_header", group.header)
         self.change_field_value("group_footer", group.footer)
+        with allure.step("Filling group creation form"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="GroupCreationFormFilled",
+                          attachment_type=AttachmentType.PNG)
 
     def select_first_group(self):
         driver = self.app.driver
         driver.find_element(By.NAME, "selected[]").click()
+        with allure.step("Selecting first group"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="SelectedFirstGroup",
+                          attachment_type=AttachmentType.PNG)
 
     def delete_first_group(self):
         driver = self.app.driver
         self.open_group_page()
         self.select_first_group()
         driver.find_element(By.NAME, "delete").click()
+        with allure.step("First group is deleted"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="DeletedFirstGroup",
+                          attachment_type=AttachmentType.PNG)
         self.return_to_group_page()
 
     def edit_first_group(self, new_group_data):
@@ -45,12 +69,32 @@ class GroupHelper:
         driver.find_element(By.NAME, "edit").click()
         self.fill_group_form(new_group_data)
         driver.find_element(By.NAME, "update").click()
+        with allure.step("First group is edited"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="EditedFirstGroup",
+                          attachment_type=AttachmentType.PNG)
 
     def return_to_group_page(self):
         driver = self.app.driver
         driver.find_element(By.LINK_TEXT, "group page").click()
+        with allure.step("Returning to group page"):
+            allure.attach(self.app.driver.get_screenshot_as_png(),
+                          name="ReturnedGroupPage",
+                          attachment_type=AttachmentType.PNG)
 
     def count(self):
         driver = self.app.driver
         self.open_group_page()
         return len(driver.find_elements(By.NAME, "selected[]"))
+
+    def get_group_list(self):
+        driver = self.app.driver
+        self.open_group_page()
+        groups_list = []
+        for group in driver.find_elements(By.CSS_SELECTOR, "span.group"):
+            text = group.text
+            id = driver.find_element(By.NAME, "selected[]").get_attribute("value")
+            groups_list.append(Group(name=text, id=id))
+        with allure.step("Got groups list"):
+            pass
+        return groups_list
